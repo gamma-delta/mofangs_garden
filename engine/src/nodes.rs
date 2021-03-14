a73ed9961adcd33a51c6a1ea4392b322c6b0fa91
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2021-03-13 19:09:18 -0600

    Fix up some node-matching logic

--- engine/src/nodes.rs
+++ engine/src/nodes.rs
@@ -38,6 +38,50 @@ impl Node {
         }
     }
 
+    /// Does this cancel as a pair with other?
+    fn cancels_with(self, other: Node) -> bool {
+        // Human cancels *all* elementals
+        if self == Node::Human && other.is_elemental()
+            || other == Node::Human && self.is_elemental() {
+            return true;
+        }
+        // One-to-one cancellations
+        let expect = match self {
+            Node::Fire =>  Some(Node::Metal),
+            Node::Metal => Some(Node::Earth),
+            Node::Earth => Some(Node::Water),
+            Node::Water => Some(Node::Wood),
+            Node::Wood => Some(Node::Fire),
+
+            Node::Heavenly => Some(Node::Yang),
+            Node::Earthly => Some(Node::Yin),
+            Node::Yang => Some(Node::Heavenly),
+            Node::Yin => Some(Node::Earthly),
+
+            Node::Qi => Some(Node::Qi),
+            Node::Change => Some(Node::Change),
+            _ => None
+        };
+        expect.filter(|o| *o == other).is_some()
+    }
+
+    /// What does Change turn this into, if any?
+    pub fn change(self) -> Option<Node> {
+        match self {
+            Node::Wood => Some(Node::Fire),
+            Node::Fire => Some(Node::Earth),
+            Node::Earth => Some(Node::Metal),
+            Node::Metal => Some(Node::Water),
+            Node::Water => Some(Node::Wood),
+
+            Node::Heavenly => Some(Node::Earthly),
+            Node::Earthly => Some(Node::Human),
+            Node::Human => Some(Node::Heavenly),
+
+            _ => None,
+        }
+    }
+
     /// Given a list of Nodes, see whether this pattern could exist and what the outcome is.
     ///
     /// - Return `Err` if this is impossible.
@@ -49,15 +93,7 @@ impl Node {
             unreachable!("You can't select 0 nodes!")
         } else if nodes.len() == 1 {
             return Ok(None);
-        } else if nodes.len() == 2 && nodes[0] == Node::Wood && nodes[1] == Node::Earth
-            || nodes[0] == Node::Fire && nodes[1] == Node::Metal
-            || nodes[0] == Node::Earth && nodes[1] == Node::Water
-            || nodes[0] == Node::Metal && nodes[1] == Node::Wood
-            || nodes[0] == Node::Water && nodes[1] == Node::Fire
-            || nodes == [Node::Qi, Node::Qi]
-        {
-            // clippy wants me to combine these...
-            // anyways, destroying elements or qi+qi cancen
+        } else if nodes.len() == 2 && nodes[0].cancels_with(nodes[1]) {
             return Ok(Some(vec![None, None]));
         } else {
             // Sort this top-to-bottom
@@ -73,9 +109,6 @@ impl Node {
             if sorted == [Node::Yin, Node::Yang] {
                 // Yin and Yang become change
                 return Ok(Some(unsort(vec![Some(Node::Change), Some(Node::Change)])));
-            } else if sorted == [Node::Change, Node::Change] {
-                // 2 Changes cancel
-                return Ok(Some(unsort(vec![None, None])));
             } else if sorted == [Node::Heavenly, Node::Earthly]
                 || sorted == [Node::Heavenly, Node::Human]
                 || sorted == [Node::Earthly, Node::Human]
@@ -84,38 +117,14 @@ impl Node {
                 return Ok(None);
             } else if sorted == [Node::Heavenly, Node::Earthly, Node::Human] {
                 // all 3 of the cycle cancel
-                return Ok(Some(unsort(vec![None, None, None])));
-            } else if sorted == [Node::Heavenly, Node::Yang]
-                || sorted == [Node::Earthly, Node::Yin]
-                || sorted.len() == 2 && sorted[0].is_elemental() && sorted[1] == Node::Human
-            {
-                // Match cycles with yin, yang, and elements
-                return Ok(Some(unsort(vec![None, None])));
+                return Ok(Some(vec![None, None, None]));
             } else if sorted.len() == 2 && sorted[0].is_elemental() && sorted[1] == Node::Qi {
                 // Qi matches with elements and turns the other into qi
                 return Ok(Some(unsort(vec![Some(Node::Qi), None])));
             } else if sorted.len() == 2 && sorted[1] == Node::Change {
                 // Change combines with lots of things
-                let operand = sorted[0];
-                if operand == Node::Change {
-                    return Ok(Some(unsort(vec![None, None])));
-                } else {
-                    let res = match operand {
-                        Node::Wood => Some(Node::Fire),
-                        Node::Fire => Some(Node::Earth),
-                        Node::Earth => Some(Node::Metal),
-                        Node::Metal => Some(Node::Water),
-                        Node::Water => Some(Node::Wood),
-
-                        Node::Heavenly => Some(Node::Earthly),
-                        Node::Earthly => Some(Node::Human),
-                        Node::Human => Some(Node::Heavenly),
-
-                        _ => None,
-                    };
-                    if let Some(res) = res {
-                        return Ok(Some(unsort(vec![Some(res), None])));
-                    }
+                if let Some(res) = sorted[0].change() {
+                    return Ok(Some(unsort(vec![Some(res), None])));
                 }
             }
         }
